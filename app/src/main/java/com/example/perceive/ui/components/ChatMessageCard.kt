dbed87da768b4c1e2d3cd4ffd7d573024255571f
technophilist <54663474+technophilist@users.noreply.github.com>
2024-02-15 18:58:56 -0500

    refactor: add ChatMessageCard composable

---
+++ app/src/main/java/com/example/perceive/ui/components/ChatMessageCard.kt
@@ -0,0 +1,47 @@
+package com.example.perceive.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+/**
+ * Represents the role of a participant in a chat conversation.
+ */
+enum class Role {
+    /**
+     * Represents the user sending the message.
+     */
+    USER,
+
+    /**
+     * Represents the participant responding to the message.
+     */
+    RESPONDER
+}
+
+/**
+ * A visually distinct card for a chat message based on the sender's [Role].
+ *
+ * @param messageContent the text content of the chat message.
+ * @param role the [Role] of the message sender
+ * @param modifier [Modifier] to be applied to the card composable.
+ */
+@Composable
+fun ChatMessageCard(messageContent: String, role: Role, modifier: Modifier = Modifier) {
+    val shape = remember(role) {
+        if (role == Role.USER) CircleShape.copy(topEnd = CornerSize(0.dp))
+        else CircleShape.copy(topStart = CornerSize(0.dp))
+    }
+    OutlinedCard(
+        modifier = modifier,
+        shape = shape,
+        content = { Text(modifier = Modifier.padding(16.dp), text = messageContent) }
+    )
+}
