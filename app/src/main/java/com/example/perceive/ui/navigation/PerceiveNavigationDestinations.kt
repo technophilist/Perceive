23b93d498e9d053e6ed45b6381129f0fcb1636a5
technophilist <54663474+technophilist@users.noreply.github.com>
2024-02-23 13:10:52 -0500

    refactor: add ChatScreen to PerceiveNavigationDestinations hierarchy

--- app/src/main/java/com/example/perceive/ui/navigation/PerceiveNavigationDestinations.kt
+++ app/src/main/java/com/example/perceive/ui/navigation/PerceiveNavigationDestinations.kt
@@ -3,4 +3,9 @@ package com.example.perceive.ui.navigation
 sealed class PerceiveNavigationDestinations(val route: String) {
     data object WelcomeScreen : PerceiveNavigationDestinations("welcome_screen")
     data object HomeScreen : PerceiveNavigationDestinations("home_screen")
+
+    data object ChatScreen : PerceiveNavigationDestinations("chat_screen/{initial_user_prompt}") {
+        const val NAV_ARG_INITIAL_USER_PROMPT = "initial_user_prompt"
+        fun buildRoute(initialUserPrompt: String) = "chat_screen/$initialUserPrompt"
+    }
 }
