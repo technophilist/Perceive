873676ca5ad1330e5fda2b2adad3cd51d18b83bd
technophilist <54663474+technophilist@users.noreply.github.com>
2024-02-23 12:59:12 -0500

    refactor!: inject & use TranscriptionService in HomeViewModel

--- app/src/main/java/com/example/perceive/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/perceive/ui/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.example.perceive.ui.home
 import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.perceive.domain.speech.TranscriptionService
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
@@ -14,7 +15,9 @@ import javax.inject.Inject
 import kotlin.time.Duration.Companion.seconds
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val transcriptionService: TranscriptionService
+) : ViewModel() {
     private val _uiState = MutableStateFlow(HomeScreenUiState())
     val uiState = _uiState as StateFlow<HomeScreenUiState>
 
@@ -24,38 +27,24 @@ class HomeViewModel @Inject constructor() : ViewModel() {
     private var currentCameraImage: Bitmap? = null
 
     // An image of what the user is currently pointing the camera at.
-    fun onStartListening(currentCameraImage: Bitmap) {
+    fun startTranscription(currentCameraImage: Bitmap) {
         this.currentCameraImage = currentCameraImage
         _userSpeechTranscriptionStream.update { "" }
-        _uiState.update {
-            HomeScreenUiState(isListening = true) // use a completely new state to remove old stale states
-        }
+        // use a completely new state to remove old stale states
+        _uiState.update { HomeScreenUiState(isListening = true) }
+        transcriptionService.startListening(
+            transcription = { transcription -> _userSpeechTranscriptionStream.update { transcription } },
+            onEndOfSpeech = ::onEndOfSpeech,
+            onError = { _uiState.update { it.copy(isListening = false, hasErrorOccurred = true) } }
+        )
     }
 
-    fun onErrorOccurred() {
-        _uiState.update {
-            it.copy(
-                isListening = false,
-                hasErrorOccurred = true
-            )
-        }
-    }
-
-    fun onUpdateTranscription(newTranscription: String) {
-        _userSpeechTranscriptionStream.update { newTranscription }
-    }
-
-    fun onStopListening() {
+    private fun onEndOfSpeech() {
         if (_userSpeechTranscriptionStream.value?.isBlank() == true || currentCameraImage == null) {
-            _uiState.update {
-                it.copy(isListening = false)
-            }
+            _uiState.update { it.copy(isListening = false) }
             return
         }
-        _uiState.update {
-            it.copy(isListening = false)
-        }
-
+        _uiState.update { it.copy(isListening = false) }
         viewModelScope.launch {
             // todo:  process request
             _uiState.update {
