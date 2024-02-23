4b7454d6321aac7c337ea1eef2b2fbf120d54f5f
technophilist <54663474+technophilist@users.noreply.github.com>
2024-02-23 13:00:07 -0500

    refactor: fix NavGraphBuilder#homeScreen to make it compatible with new changes in HomeViewModel

--- app/src/main/java/com/example/perceive/ui/PerceiveApp.kt
+++ app/src/main/java/com/example/perceive/ui/PerceiveApp.kt
@@ -12,7 +12,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
@@ -31,7 +34,7 @@ import com.example.perceive.utils.takePicture
 fun PerceiveApp(navHostController: NavHostController = rememberNavController()) {
     NavHost(
         navController = navHostController,
-        startDestination = PerceiveNavigationDestinations.WelcomeScreen.route
+        startDestination = PerceiveNavigationDestinations.HomeScreen.route
     ) {
         composable(route = PerceiveNavigationDestinations.WelcomeScreen.route) {
             // TODO: make welcome screen visible only once
@@ -52,52 +55,15 @@ private fun NavGraphBuilder.homeScreen(route: String) {
         val uiState by homeViewModel.uiState.collectAsStateWithLifecycle()
         val cameraController = remember { LifecycleCameraController(context) }
         val transcription by homeViewModel.userSpeechTranscriptionStream.collectAsStateWithLifecycle()
-        val speechRecognizer = remember { SpeechRecognizer.createSpeechRecognizer(context) }
-        val speechRecognizerIntent = remember {
-            Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
-                putExtra(
-                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-                )
-                putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
-            }
-        }
-        // An image of what the user is currently pointing the camera at.
-        var currentCameraImage by remember { mutableStateOf<Bitmap?>(null) }
-        val listener = remember {
-            createRecognitionListener(
-                onReadyForSpeech = {
-                    homeViewModel.onStartListening(
-                        currentCameraImage ?: return@createRecognitionListener
-                    )
-                },
-                onEndOfSpeech = {
-                    speechRecognizer.stopListening()
-                    speechRecognizer.destroy()
-                    homeViewModel.onStopListening()
-                },
-                onError = { homeViewModel.onErrorOccurred() },
-                onPartialResults = { partialResultsBundle ->
-                    val transcript = partialResultsBundle
-                        ?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                        ?.first() ?: return@createRecognitionListener
-                    homeViewModel.onUpdateTranscription(transcript)
-                }
-            )
-        }
         HomeScreen(
             cameraController = cameraController,
             transcriptionText = transcription,
             homeScreenUiState = uiState,
             onStartListening = {
                 cameraController.takePicture(
-                    context,
-                    onSuccess = {
-                        currentCameraImage = it.toBitmap()
-                        speechRecognizer.setRecognitionListener(listener)
-                        speechRecognizer.startListening(speechRecognizerIntent)
-                    },
-                    onError = { homeViewModel.onErrorOccurred() }
+                    context = context,
+                    onSuccess = { homeViewModel.startTranscription(it.toBitmap()) },
+                    onError = { /*TODO*/ }
                 )
             }
         )
