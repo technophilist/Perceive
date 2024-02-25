1dc7c26797d348582d3ebac04f57b07bdd7f122b
technophilist <54663474+technophilist@users.noreply.github.com>
2024-02-24 19:35:37 -0500

    refactor: add CoroutineDispatchersModule (di module)

---
+++ app/src/main/java/com/example/perceive/di/CoroutineDispatchersModule.kt
@@ -0,0 +1,37 @@
+package com.example.perceive.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Qualifier
+annotation class MainDispatcher
+
+@Qualifier
+annotation class IODispatcher
+
+@Qualifier
+annotation class DefaultDispatcher
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineDispatchersModule {
+    @Provides
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @IODispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @MainDispatcher
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
