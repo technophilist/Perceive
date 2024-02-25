13f1efa04b84402752c3a720d221303877b85ab3
technophilist <54663474+technophilist@users.noreply.github.com>
2024-02-24 19:31:53 -0500

    refactor: add BitmapStore interface

---
+++ app/src/main/java/com/example/perceive/data/local/bitmapstore/BitmapStore.kt
@@ -0,0 +1,28 @@
+package com.example.perceive.data.local.bitmapstore
+
+import android.graphics.Bitmap
+import android.net.Uri
+
+
+/**
+ * A store for saving and retrieving [Bitmap]s.
+ */
+interface BitmapStore {
+    /**
+     * Saves the given [bitmap] and returns the [Uri] of the saved bitmap.
+     * Returns `null` if the bitmap could not be saved.
+     */
+    suspend fun saveBitmap(bitmap: Bitmap): Uri?
+
+    /**
+     * Retrieves a [Bitmap] for the given [uri].
+     * Returns `null` if the bitmap could not be retrieved or if it doesn't exist.
+     */
+    suspend fun retrieveBitmapForUri(uri: Uri): Bitmap?
+
+    /**
+     * Deletes the bitmap with the given [uri].
+     * Returns `true` if the bitmap was deleted successfully, `false` otherwise.
+     */
+    suspend fun deleteBitmapWithUri(uri: Uri): Boolean
+}
