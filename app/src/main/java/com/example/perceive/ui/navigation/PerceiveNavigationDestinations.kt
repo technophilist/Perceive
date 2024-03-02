58ce2f31d3dc8886515da63f9e00f2807499e474
technophilist <54663474+technophilist@users.noreply.github.com>
2024-03-02 17:52:55 -0500

    refactor: add ChatScreen to PerceiveNavigationDestinations

--- app/src/main/java/com/example/perceive/ui/navigation/PerceiveNavigationDestinations.kt
+++ app/src/main/java/com/example/perceive/ui/navigation/PerceiveNavigationDestinations.kt
@@ -1,11 +1,20 @@
 package com.example.perceive.ui.navigation
 
+import android.net.Uri
+import java.util.Base64
+
 sealed class PerceiveNavigationDestinations(val route: String) {
     data object WelcomeScreen : PerceiveNavigationDestinations("welcome_screen")
     data object HomeScreen : PerceiveNavigationDestinations("home_screen")
 
-    data object ChatScreen : PerceiveNavigationDestinations("chat_screen/{initial_user_prompt}") {
+    data object ChatScreen :
+        PerceiveNavigationDestinations("chat_screen/{initial_user_prompt}/{associated_image_uri_base64}") {
         const val NAV_ARG_INITIAL_USER_PROMPT = "initial_user_prompt"
-        fun buildRoute(initialUserPrompt: String) = "chat_screen/$initialUserPrompt"
+        const val NAV_ARG_ASSOCIATED_IMAGE_URI_BASE64 = "associated_image_uri_base64"
+        fun buildRoute(initialUserPrompt: String, associatedImageUri: Uri): String {
+            val imageUriByteArray = associatedImageUri.toString().toByteArray()
+            val encodedImageUri = Base64.getEncoder().encodeToString(imageUriByteArray)
+            return "chat_screen/$initialUserPrompt/$encodedImageUri"
+        }
     }
 }
