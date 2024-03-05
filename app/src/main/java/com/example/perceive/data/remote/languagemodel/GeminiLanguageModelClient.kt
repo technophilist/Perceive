58c7822868b6278c723242fc75e4a5a6a7df9152
technophilist <54663474+technophilist@users.noreply.github.com>
2024-03-05 18:45:49 -0500

    refactor: use workaround in GeminiLanguageModelClient to enable multi-model, multi-turn conversations

--- app/src/main/java/com/example/perceive/data/remote/languagemodel/GeminiLanguageModelClient.kt
+++ app/src/main/java/com/example/perceive/data/remote/languagemodel/GeminiLanguageModelClient.kt
@@ -2,61 +2,68 @@ package com.example.perceive.data.remote.languagemodel
 
 
 import android.graphics.Bitmap
-import androidx.compose.runtime.internal.illegalDecoyCallException
 import com.example.perceive.BuildConfig
 import com.google.ai.client.generativeai.Chat
 import com.google.ai.client.generativeai.GenerativeModel
-import com.google.ai.client.generativeai.type.Content
 import com.google.ai.client.generativeai.type.content
 import kotlinx.coroutines.CancellationException
 import java.lang.IllegalStateException
 import javax.inject.Inject
 
 class GeminiLanguageModelClient @Inject constructor() : MultiModalLanguageModelClient {
-    private val generativeModel = GenerativeModel(
-        modelName = GEMINI_PRO_VISION,
+    private val generativeTextModel = GenerativeModel(
+        modelName = GeminiModels.GEMINI_PRO,
         apiKey = BuildConfig.GOOGLE_GEMINI_API_KEY
     )
 
+    private val generativeMultiModalModel by lazy {
+        GenerativeModel(
+            modelName = GeminiModels.GEMINI_PRO_VISION,
+            apiKey = BuildConfig.GOOGLE_GEMINI_API_KEY
+        )
+    }
+
     private var currentChatSession: Chat? = null
 
-    // At the time of writing this, Gemini-pro-vision requires at least 1
-    // image in the conversation history. Else, an exception would be
-    // thrown. However, conversations may not always start with an image.
-    // Use the following variable to insert an "empty" bitmap in conversation
-    // history in case the conversation history doesn't have any bitmaps in it yet.
-    private val emptyBitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888)
-    private var doesCurrentChatSessionHasOneImage = false
-
-
     override fun startNewChatSession() {
-        currentChatSession = generativeModel.startChat()
+        currentChatSession = generativeTextModel.startChat()
     }
 
     override suspend fun sendMessage(messageContents: List<MultiModalLanguageModelClient.MessageContent>): Result<String> {
-        val content = content(role = "user") {
-            messageContents.forEach { messageContent ->
-                when (messageContent) {
-                    is MultiModalLanguageModelClient.MessageContent.Image -> image(messageContent.bitmap)
-                    is MultiModalLanguageModelClient.MessageContent.Text -> text(messageContent.text)
-                }
-            }
-            // At the time of writing this, Gemini-pro-vision requires at least 1
-            // image in the conversation history. Else, an exception would be
-            // thrown. However, conversations may not always start with an image.
-            // Hence, to circumvent this limitation, add an empty bitmap and set
-            // the flag to true.
-            if (!doesCurrentChatSessionHasOneImage) {
-                image(emptyBitmap)
-                doesCurrentChatSessionHasOneImage = true
-            }
-        }
+        val currentChatSession =
+            currentChatSession ?: throw IllegalStateException(CHAT_SESSION_INVALID_MESSAGE)
 
         return try {
-            val currentChatSession =
-                currentChatSession ?: throw IllegalStateException(CHAT_SESSION_INVALID_MESSAGE)
-
-            Result.success(currentChatSession.sendMessage(content).text!!)
+            val imagesInContent: List<Bitmap> = messageContents.mapNotNull {
+                if (it !is MultiModalLanguageModelClient.MessageContent.Image) return@mapNotNull null
+                it.bitmap
+            }
+            val textMessagesInContent = messageContents.mapNotNull {
+                if (it !is MultiModalLanguageModelClient.MessageContent.Text) return@mapNotNull null
+                it.text
+            }
+            var descriptionOfImages: String? = null
+            // At the time of writing, gemini-pro-vision doesn't support multi-turn conversations.
+            // To circumvent this, we generate a description of the images using gemini-pro-vision
+            // and use it as the prompt for the gemini-pro model.
+            if (imagesInContent.isNotEmpty()) {
+                val content = content(GeminiRoles.USER) {
+                    imagesInContent.forEach { image(it) }
+                    text("Provide a detailed description of the contents of the image(s).")
+                }
+                descriptionOfImages = generativeMultiModalModel.generateContent(content).text ?: ""
+            }
+            val prompt = content(GeminiRoles.USER) {
+                descriptionOfImages?.let {
+                    text(
+                        "Given the following description of image(s), answer the question(s) that follow, in a conversational manner." +
+                                "Try to answer without using sentences that contain the word \"photo\"."
+                    )
+                    text(it)
+                }
+                textMessagesInContent.forEach { text(it) }
+            }
+            Result.success(currentChatSession.sendMessage(prompt).text!!)
         } catch (exception: Exception) {
             if (exception is CancellationException) throw exception
             Result.failure(exception)
@@ -65,11 +72,19 @@ class GeminiLanguageModelClient @Inject constructor() : MultiModalLanguageModelC
 
     override fun endChatSession() {
         currentChatSession = null
-        doesCurrentChatSessionHasOneImage = false
     }
 
     private companion object {
-        const val GEMINI_PRO_VISION = "gemini-pro-vision"
+        object GeminiModels {
+            const val GEMINI_PRO_VISION = "gemini-pro-vision"
+            const val GEMINI_PRO = "gemini-pro"
+        }
+
+        object GeminiRoles {
+            const val USER = "user"
+            const val MODEL = "model"
+        }
+
         const val CHAT_SESSION_INVALID_MESSAGE =
             "A chat session is not in progress. Please use startNewChat() before attempting to send new messages."
     }
