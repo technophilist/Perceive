1cd346abe5192e8dfd47469254455975dec58e54
technophilist <54663474+technophilist@users.noreply.github.com>
2024-03-09 19:23:42 -0500

    refactor: add AndroidTextToSpeechService

---
+++ app/src/main/java/com/example/perceive/domain/speech/tts/AndroidTextToSpeechService.kt
@@ -0,0 +1,45 @@
+package com.example.perceive.domain.speech.tts
+
+import android.content.Context
+import android.speech.tts.TextToSpeech
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+import java.util.Locale
+import javax.inject.Inject
+
+/**
+ * A [TextToSpeechService] implementation that uses the Android [TextToSpeech] under the hood
+ * to speak text.
+ */
+class AndroidTextToSpeechService @Inject constructor(
+    private val context: Context
+) : TextToSpeechService {
+
+    private var textToSpeech: TextToSpeech? = null
+
+    override fun startSpeaking(
+        text: String,
+        onFailure: (Exception) -> Unit,
+        onSuccess: (() -> Unit)?
+    ) {
+        if (text.length > TextToSpeech.getMaxSpeechInputLength()) {
+            onFailure(IllegalArgumentException("The text length is larger than the max supported speech input length"))
+            return
+        }
+        textToSpeech = TextToSpeech(context) { status ->
+            if (status != TextToSpeech.SUCCESS) {
+                onFailure(Exception("Failed to initialize TextToSpeech"))
+                return@TextToSpeech
+            }
+            textToSpeech?.language = Locale.US
+            val speakResult = textToSpeech?.speak(text, TextToSpeech.QUEUE_ADD, null, null)
+            if (speakResult == TextToSpeech.ERROR) onFailure(Exception("An internal error occurred while attempting to speak"))
+            else onSuccess?.invoke()
+        }
+    }
+
+    override fun stopSpeakingAndClearResources() {
+        textToSpeech?.stop()
+        textToSpeech?.shutdown()
+    }
+}
