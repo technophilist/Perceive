35175b23d9b13994ea6f20d66c9e817edc14df6c
technophilist <54663474+technophilist@users.noreply.github.com>
2024-03-28 20:20:12 -0400

    refactor: add di binding for preferences datastore

--- app/src/main/java/com/example/perceive/di/LocalDataModule.kt
+++ app/src/main/java/com/example/perceive/di/LocalDataModule.kt
@@ -1,11 +1,20 @@
 package com.example.perceive.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.example.perceive.data.local.bitmapstore.AndroidBitmapStore
 import com.example.perceive.data.local.bitmapstore.BitmapStore
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 
 @Module
@@ -14,4 +23,16 @@ abstract class LocalDataModule {
 
     @Binds
     abstract fun bindBitmapStore(impl: AndroidBitmapStore): BitmapStore
+
+    companion object {
+        @Provides
+        @Singleton
+        fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+            return PreferenceDataStoreFactory.create {
+                context.preferencesDataStoreFile(PERCEIVE_USER_PREFERENCES_STORE_NAME)
+            }
+        }
+    }
 }
+
+private const val PERCEIVE_USER_PREFERENCES_STORE_NAME = "perceive_user_preferences"
