3da9715bcfe53d2f106b9ced1c732ccd6b56e159
technophilist <54663474+technophilist@users.noreply.github.com>
2024-03-30 12:12:03 -0400

    fix: move preferences bindings to separate module to fix wrongly scoped binding

--- app/src/main/java/com/example/perceive/di/PreferencesModule.kt
+++ app/src/main/java/com/example/perceive/di/PreferencesModule.kt
@@ -4,25 +4,33 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.datastore.preferences.preferencesDataStoreFile
-import com.example.perceive.data.local.bitmapstore.AndroidBitmapStore
-import com.example.perceive.data.local.bitmapstore.BitmapStore
 import com.example.perceive.data.preferences.PerceiveUserPreferencesManager
 import com.example.perceive.data.preferences.UserPreferencesManager
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-
 @Module
-@InstallIn(ViewModelComponent::class)
-abstract class LocalDataModule {
+@InstallIn(SingletonComponent::class)
+abstract class PreferencesModule {
 
     @Binds
-    abstract fun bindBitmapStore(impl: AndroidBitmapStore): BitmapStore
+    abstract fun bindPreferencesManager(impl: PerceiveUserPreferencesManager): UserPreferencesManager
+
+    companion object {
+        @Provides
+        @Singleton
+        fun providePreferencesDataStore(
+            @ApplicationContext context: Context
+        ): DataStore<Preferences> = PreferenceDataStoreFactory.create {
+            context.preferencesDataStoreFile(USER_PREFERENCES_STORE_NAME)
+        }
+    }
 }
+
+private const val USER_PREFERENCES_STORE_NAME = "perceive_user_preferences"
