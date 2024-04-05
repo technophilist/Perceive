c5841720efb79075f2a1f78bbbc4bafe8647d29a
technophilist <54663474+technophilist@users.noreply.github.com>
2024-04-05 19:33:42 -0400

    refactor: make WelcomeScreen visible only once after installation

--- app/src/main/java/com/example/perceive/MainActivity.kt
+++ app/src/main/java/com/example/perceive/MainActivity.kt
@@ -1,35 +1,19 @@
 package com.example.perceive
 
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.speech.RecognitionListener
-import android.speech.RecognizerIntent
-import android.speech.SpeechRecognizer
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
-import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.perceive.ui.PerceiveApp
-import com.example.perceive.ui.home.HomeScreen
-import com.example.perceive.ui.home.HomeViewModel
 import com.example.perceive.ui.theme.PerceiveTheme
-import com.example.perceive.utils.createRecognitionListener
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -39,12 +23,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            PerceiveTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                    content = { PerceiveApp() }
+            PerceiveTheme(content = { MainActivityContent() })
+        }
+    }
+
+    @Composable
+    private fun MainActivityContent() {
+        val mainViewModel = hiltViewModel<MainViewModel>()
+        val shouldShowWelcomeScreenState by mainViewModel
+            .shouldDisplayWelcomeScreenStream
+            .collectAsStateWithLifecycle()
+        shouldShowWelcomeScreenState?.let { shouldShowWelcomeScreen ->
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background,
+            ) {
+                PerceiveApp(
+                    shouldShowWelcomeScreen = shouldShowWelcomeScreen,
+                    onNavigateToHomeScreen = mainViewModel::onNavigateToHomeScreen
                 )
             }
         }
