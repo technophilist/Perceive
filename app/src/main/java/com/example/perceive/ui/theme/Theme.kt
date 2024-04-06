72fbf16376884526513151d7da0aa2944a2113de
technophilist <54663474+technophilist@users.noreply.github.com>
2024-04-05 20:09:58 -0400

    refactor: completely revamp app's theme & add support for dynamic colors

--- app/src/main/java/com/example/perceive/ui/theme/Theme.kt
+++ app/src/main/java/com/example/perceive/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.perceive.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,18 +8,83 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme()
+private val DarkColorScheme = darkColorScheme(
+    primary = Blue80,
+    onPrimary = Blue20,
+    primaryContainer = Blue30,
+    onPrimaryContainer = Blue90,
+    inversePrimary = Blue40,
+    secondary = DarkBlue80,
+    onSecondary = DarkBlue20,
+    secondaryContainer = DarkBlue30,
+    onSecondaryContainer = DarkBlue90,
+    tertiary = Yellow80,
+    onTertiary = Yellow20,
+    tertiaryContainer = Yellow30,
+    onTertiaryContainer = Yellow90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = Grey10,
+    onBackground = Grey90,
+    surface = Grey10,
+    onSurface = Grey80,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey20,
+    surfaceVariant = BlueGrey30,
+    onSurfaceVariant = BlueGrey80,
+    outline = BlueGrey60
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Blue40,
+    onPrimary = Color.White,
+    primaryContainer = Blue90,
+    onPrimaryContainer = Blue10,
+    inversePrimary = Blue80,
+    secondary = DarkBlue40,
+    onSecondary = Color.White,
+    secondaryContainer = DarkBlue90,
+    onSecondaryContainer = DarkBlue10,
+    tertiary = Yellow40,
+    onTertiary = Color.White,
+    tertiaryContainer = Yellow90,
+    onTertiaryContainer = Yellow10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = Grey99,
+    onSurface = Grey10,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = BlueGrey90,
+    onSurfaceVariant = BlueGrey30,
+    outline = BlueGrey50
+)
 
 @Composable
-fun PerceiveTheme(content: @Composable () -> Unit) {
+fun PerceiveTheme(
+    isDarkThemeEnabled: Boolean = isSystemInDarkTheme(),
+    enableDynamicColorsIfSupported: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val isDynamicColorsFeatureSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = if (isDynamicColorsFeatureSupported && enableDynamicColorsIfSupported) {
+        val context = LocalContext.current
+        if (isDarkThemeEnabled) dynamicDarkColorScheme(context = context)
+        else dynamicLightColorScheme(context = context)
+    } else {
+        if (isDarkThemeEnabled) DarkColorScheme else LightColorScheme
+    }
     MaterialTheme(
-        colorScheme = DarkColorScheme,
+        colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
